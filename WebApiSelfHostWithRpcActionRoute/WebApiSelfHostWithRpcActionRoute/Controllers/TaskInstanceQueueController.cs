a27952c322f7e3950c384df507dc86e6b1caba62
Mark Jones <mark@jones.io>
2012-09-12 11:23:40 +0100

    Adding example of self host with action verb

---
+++ WebApiSelfHostWithRpcActionRoute/WebApiSelfHostWithRpcActionRoute/Controllers/TaskInstanceQueueController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApiSelfHostWithRpcActionRoute.Controllers
+{
+    public class TaskInstanceQueueController : ApiController
+    {
+
+        public void Get(string id)
+        {
+            // Do something with my taskInstance
+            Console.WriteLine("Method entered!" + id);
+        }
+
+        public void Queue(TaskInstance taskInstance)
+        {
+            // Do something with my taskInstance
+            Console.WriteLine("Method entered!");
+        }
+    }
+}
---
+++ WebApiSelfHostWithRpcActionRoute/WebApiSelfHostWithRpcActionRoute/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace WebApiSelfHostWithRpcActionRoute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up server configuration 
+            HttpSelfHostConfiguration config = new HttpSelfHostConfiguration("http://localhost:8080");
+
+            config.Routes.MapHttpRoute("API Default", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+            config.Routes.MapHttpRoute("API RPC Style", "api/{controller}/{action}", new { id = RouteParameter.Optional });
+
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
