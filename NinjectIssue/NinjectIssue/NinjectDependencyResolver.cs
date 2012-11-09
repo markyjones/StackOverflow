44dbf48b3408c519de85f569f3ee1d0201f0674a
Mark Jones <mark@jones.io>
2012-11-09 22:50:57 +0000

    Adding an example ninject for weapi project for question http://stackoverflow.com/questions/13298153/ninject-ninject-extensions-with-web-api-does-not-resolve-types

---
+++ NinjectIssue/NinjectIssue/NinjectDependencyResolver.cs
@@ -0,0 +1,25 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Dependencies;
+
+namespace NinjectIssue
+{
+    public class NinjectDependencyResolver : NinjectDependencyScope, IDependencyResolver
+    {
+        readonly IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+            : base(kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new NinjectDependencyScope(this.kernel.BeginBlock());
+        }
+    }
+}
